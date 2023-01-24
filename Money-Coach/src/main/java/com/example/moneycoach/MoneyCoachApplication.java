da730bf9bc2910e354faface1b1191aedf5fc17a
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-01-24 17:40:52 -0600

    Feat: Spring Project

---
+++ Money-Coach/src/main/java/com/example/moneycoach/MoneyCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneycoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyCoachApplication.class, args);
+    }
+
+}
---
+++ Money-Coach/src/test/java/com/example/moneycoach/MoneyCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneycoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
