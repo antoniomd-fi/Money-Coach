bd3934f571963bf6e90c5a42dc3e19b0d0017c1f
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-02-27 23:37:36 -0600

    Update

--- MoneyCoach/src/main/java/com/example/moneycoach/config/CorsConfiguration.java
+++ MoneyCoach/src/main/java/com/example/moneycoach/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://money-coach.netlify.app") // Cambiar por el dominio que esté haciendo la solicitud
+                        .allowedOrigins("http://localhost:3000") // Cambiar por el dominio que esté haciendo la solicitud
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
